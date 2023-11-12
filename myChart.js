bf3a2914ee698fa6a1895bde6fb6c8e2b3fad545
“Jessica <jmychalm13@gmail.com>
2023-11-12 13:48:50 -0600

    chartJS working, somehow

---
+++ myChart.js
@@ -0,0 +1,95 @@
+// Your JavaScript code for creating the chart will go here
+var ctx = document.getElementById("myChart").getContext("2d");
+// eslint-disable-next-line no-undef
+var myChart = new Chart(ctx, {
+  type: "doughnut",
+  data: {
+    labels: ["Ruby", "JavaScript", "HTML", "SQL", "CSS"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+var ctx2 = document.getElementById("toolbox").getContext("2d");
+
+var toolbox = new Chart(ctx2, {
+  type: "pie",
+  data: {
+    labels: [
+      "Bootstrap",
+      "React",
+      "Ruby on Rails",
+      "Tailwind",
+      "Minitest",
+      "Postgres",
+      "APIs",
+      "Node/npm",
+      "Git",
+      "GitHub",
+      "Postman",
+      "Postico",
+      "AWS Amplify",
+    ],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          "blue",
+          "purple",
+          "pink",
+          "violet",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
